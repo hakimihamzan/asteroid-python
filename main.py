0c284036b6cdd35b3cedbe7e6df3fe08b13ba5f4
hakimihamzan <hakimihamzan20@gmail.com>
2025-09-23 21:11:11 +0800

    Reorder initialization of Player and AsteroidField containers for clarity

--- main.py
+++ main.py
@@ -17,12 +17,13 @@ def main():
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
 
-    Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = updatable
+    asteroidfield = AsteroidField()
+
+    Player.containers = (updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    asteroidfield = AsteroidField()
 
     while True:
         for event in pygame.event.get():
